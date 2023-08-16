2dc4f00ec5027c30caa6be517f67ee1cfa814524
Ben <benmotykax@gmail.com>
2023-08-16 19:55:16 +0200

    add main loop

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 #![allow(unused)]
 
 use clap::{Parser, Subcommand};
+use std::thread;
+use std::time::Duration;
+use std::process::Command;
 
 #[derive(Parser)]
 #[command(name = "AirPods battery checker")]
 #[command(about = "Short about", long_about = "Long about")]
 struct Cli {
     #[clap(short, long, default_value = "1")]
-    interval: i8,
+    interval: u8,
 }
 
 fn main() {
     let args = Cli::parse();
+
+    loop {
+        clear_console();
+        println!("Battery status:");
+
+        thread::sleep(Duration::from_secs(args.interval as u64));
+    }
     println!("{}", args.interval);
+}
+
+fn clear_console() {
+    if cfg!(target_os = "windows") {
+        let _ = Command::new("cmd").arg("/c").arg("cls").status();
+    } else {
+        let _ = Command::new("clear").status();
+    }
 }
